dc6ea508a5b10b50a0564292db08bd567bf6d24d
Beyonce Knowles <beyonce@thinkful.com>
2016-12-19 21:38:01 +0000

    added get()

--- snippets.py
+++ snippets.py
@@ -1,61 +1,74 @@
-import psycopg2
-import logging 
+import logging
 import argparse
+import psycopg2
 
-
+# Set the log output file and the log level
+logging.basicConfig(filename = "snippets.log", level = logging.DEBUG)
 logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect(database="snippets")
-logging.debug("Datbaase connection established.")
+logging.debug("Database connection established.")
 
-
-#Set the log output file and the log level 
-logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 def put(name, snippet):
-   """Store a snippet with an associated name."""
-   logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-   cursor = connection.cursor()
-   command = "insert into snippets values (%s, %s)"
-   cursor.execute(command, (name, snippet))
-   connection.commit()
-   logging.debug("Snippet stored successfully.")
-   return name, snippet
+    """
+    Store a snippet with an associated name.
+    Returns the name and the snippet
+    """
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    with connection, connection.cursor() as cursor:
+        try:
+            cursor.execute("INSERT INTO snippets VALUES (%s, %s)", (name, snippet))
+        except psycopg2.IntegrityError as e:
+            cursor.execute("update snippets set message = %s where keyword = %s", (snippet, name))
+    logging.debug("Snippet stored successfully.")
+    return name, snippet
+
 
 def get(name):
-    """Retrieve the snippet with a given name. 
-    
+    """Retrieve the snippet with a given name.
     If there is no such snippet, return '404: Snippet Not Found'.
-    
-    Returns the snippet.Returns
+    Returns the snippet.
     """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
-    
+    logging.info("Retrieving snippet {!r}".format(name))
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        snippet = cursor.fetchone()  # Have to assign return value to variable
+    logging.debug("Snippet retrieved successfully.")
+    if not snippet: # No snippet was found with that keyword name
+        return "404: Snippet not found"
+    else:
+        return snippet[0]   # Return only the snippet
+
+
 def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    
-    subparsers = parser.add_subparsers(dest="Command", help="Available Commands")
-    
-    #subparser for put command 
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
     logging.debug("Constructing put subparser")
-    put_parser = subparsers.add_parser("put" , help="Store a snippet")
-    put_parser.add_argument("name", help="Name of snippet")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
-    
+
+    # Subparser for get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help = "Return a snippet")
+    get_parser.add_argument("name", help = "Name of the snippet")
+
     arguments = parser.parse_args()
-    #convert parsed arguments from Namespace to dictionary
+    # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
     command = arguments.pop("command")
-    
+
     if command == "put":
         name, snippet = put(**arguments)
         print("Stored {!r} as {!r}".format(snippet, name))
     elif command == "get":
-            snippet = get(**arguments)
-            print("Retrieved snippet: {!r}.".format(snippet))
-    
-    
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+
+
 if __name__ == "__main__":
-    main()  
+    main()
